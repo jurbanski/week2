64b491290c995e535ebf31704ddd76378bca834c
Joseph Urbanski <jurbanski@ci.uchicago.edu>
2015-01-13 17:00:19 -0600

    First working version.

--- solution_1.py
+++ solution_1.py
@@ -3,4 +3,19 @@
 # Joseph Urbanski
 # MPCS 50101
 
+index = eval(input("What Fibonacci number do you want to find:\n"))
+numFib = 1
+num1 = 1
+num2 = 1
+
+if index <= 0
+	index = eval(input("Please input a positive integer:\n"))
+
+while index > 2:
+	numFib = num1 + num2
+	num1 = num2
+	num2 = numFib
+	index -= 1
+
+print("Your Fibonnaci number is ", numFib, ".", sep="")
 
