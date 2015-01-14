ac765ecc4663174673be148517935a5776ae9ce4
Joseph Urbanski <jurbanski@ci.uchicago.edu>
2015-01-14 17:00:08 -0600

    Cleaned up the user input with a function, corrected MPG calculating code.

--- solution_10.py
+++ solution_10.py
@@ -5,44 +5,71 @@
 # Week 2 homework
 # Chapter 8, Question 10 solution.
 
+# Import the sys module to allow us to read the input filename from command line arguments
 import sys
 
-# Set the filename variable to an empty string to avoid scope problems with the try block below
-filename = ''
+# This function will prompt the user for a filename, then check to see if the file exists.  If the file does not exist, it prompts again.  If the file does exist, it returns the name of the file.
+def prompt_for_filename():
+        while True:
+                filename = input("Please enter an input filename: ")
+                try:
+                        infile = open(filename, 'r')
+                        return filename
+                except OSError:
+                        print("File not found!")
 
-# Check to see if the filename has been provided as a command line argument, if it hasn't prompt for user input and check to see if that file actually exists
+# Check to see if the filename has been provided as a command line argument, if it hasn't, prompt for user input with the prompt_for_filename() function.
 try:
 	filename = sys.argv[1]
 	infile = open(filename, 'r')
+	# CASE: File not found.
 except OSError:
 	print("File not found!")
+	filename = prompt_for_filename()
+	# CASE: No filename provided on the command line.
 except IndexError:
-	while True:
-		filename = input("Please enter an input filename:")
-		try:
-			infile = open(filename, 'r')
-			break
-		except OSError:
-			print("File not found!")
+	filename = prompt_for_filename()
 
-# infile won't be set when the try block above finishes (out of scope), so set it here
+# infile may not be set when the try block above finishes (out of scope if we have to prompt for a filename with prompt_for_filename()), so set it here.
 infile = open(filename, 'r')
 
+# Initialize some working variables.
 leg = 0
 odometer_prev = 0
-mpg_total = 0
+miles_total = 0
+gas_total = 0
 
+# Loop over every line in the file.
 for line in infile:
+	# Split the current line into an array of strings.
 	currline = line.split()
-	odometer_curr = int(currline[0])
+	# Set the current odometer to the first value in the current line.
+	odometer_curr = float(currline[0])
+	# Test to see if this is the first line in the file.
 	if leg == 0:
+		# If so, then:
+		# Set the previous odometer to be the current odometer.
 		odometer_prev = odometer_curr
+		# Increment the leg counter.
 		leg += 1
 	else:
-		gas_used = int(currline[1])
-		mpg_curr = (odometer_curr - odometer_prev) / gas_used
+		# If not the first line, then:
+		# Set the gas used to the second item in the current line.
+		gas_used = float(currline[1])
+		# Determine the miles traveled from the two odometer values.
+		miles_traveled = odometer_curr - odometer_prev
+		# Add the miles traveled on the current leg to the total.
+		miles_total += miles_traveled
+		# Add the gas used to the total.
+		gas_total += gas_used
+		# Calculate the MPG for this leg of the trip.
+		mpg_curr = miles_traveled / gas_used
+		# Output the MPG for this leg.
 		print("Leg", leg, "MPG:", round(mpg_curr, 2))
-		mpg_total += mpg_curr
+		# Set the previous odometer to the current odometer for the next loop
+		odometer_prev = odometer_curr
+		# Increment the leg counter.
 		leg += 1
 
-print("Total MPG:", round(mpg_total, 2))
+# Finally, calculate the total average MPG for the trip and output that value.
+print("Total MPG:", round(miles_total / gas_total, 2))
