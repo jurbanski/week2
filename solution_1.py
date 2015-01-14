11b35e416949cc786084f1c9efb5c153111dbf2a
Joseph Urbanski <jurbanski@ci.uchicago.edu>
2015-01-14 17:03:03 -0600

    Changed variable name from camel caps to underscores.

--- solution_1.py
+++ solution_1.py
@@ -20,19 +20,19 @@ while index <= 0:
                 print("INVALID INPUT!")
 
 # Initialize variables.  
-# numFib will be used as a place holder in the calculation, but will also hold the final output.  Setting this to one allows us to skip the calculation loop for any number less than 3.
-numFib = 1
+# num_fib will be used as a place holder in the calculation, but will also hold the final output.  Setting this to one allows us to skip the calculation loop for any number less than 3.
+num_fib = 1
 # The first two Fibonacci numbers.  Will also be used in the calculation.
 num1 = 1
 num2 = 1
 
 # Calculate the Fibonacci number.  Any number less than 3 will be '1', so only loop for numbers greater than that.
 while index > 2: 
-	numFib = num1 + num2
+	num_fib = num1 + num2
 	num1 = num2
-	num2 = numFib
+	num2 = num_fib
 	index -= 1
 
 # Output the Fibonacci number.
-print("Your Fibonacci number is ", numFib, ".", sep="")
+print("Your Fibonacci number is ", num_fib, ".", sep="")
 
