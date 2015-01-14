01c45c3b99495e310b43215a8695f6239d5b72c9
Joseph Urbanski <jurbanski@ci.uchicago.edu>
2015-01-13 18:50:13 -0600

    Adding try:except code to screen for string input

--- solution_4.py
+++ solution_4.py
@@ -5,36 +5,30 @@
 # Week 2 homework
 # Chapter 8, Question 4 solution.
 
+# Initialize the variable that will hold our input, setting to zero so the while loop executes below.
 num = 0
 
-# Prompt for input.
+# Prompt for input, looping until we get a positive integer...
 while num <= 0:
 	try:
 		num = int(input("Please enter a positive integer:\n"))
+		# ... while testing for non-positive integers...
 		if num <= 0:
 			print("That's not a POSITIVE INTEGER!  ", end="")
+	# ... and any string input.
 	except ValueError:
 		print("That's not even a number!  ", end="")
-
-
-# Screen input for strings and reprompt if necessary.
-#while isinstance(num, str):
-#	num = input("Please input a POSITIVE integer:\n")
-
-# Screen input for non-positive integers and re-prompt for input if necessary.
-#while num <= 0:
-#	num = int(input("Please input a POSITIVE integer:\n"))
 
 # Output the start of our final output...
 print("Your Syracuse sequence starting with ", num, " is: ", num, sep="", end="")
 
-# ..then calculate the sequence, outputting the number at each iteration.
+# ... then calculate the sequence, outputting the number at each iteration.
 while num != 1:
 	print(", ", end="")  # Output the sequence separator text first.
 	# If even, divide by two...
 	if num % 2 == 0:
 		num = int(num / 2)
-	# ...if odd, multiply by three and add one.
+	# ... if odd, multiply by three and add one.
 	else:
 		num = int(3 * num + 1)
 	# Output the number in the sequence.
